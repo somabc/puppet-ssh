f004f261aab07661885cb14a8ea8d6f81cc76e6e
root <root@bryanc1.mylabserver.com>
2018-09-15 17:24:54 +0000

    OS Family

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,27 +1,34 @@
 class ssh::params {
-  if $facts['os']['family'] == 'Debian' {
-    $package_name = 'openssh-server'
-    $service_name = 'ssh'
-  }
-  elsif $facts['os']['family'] == 'Redhat' {
-    $package_name = 'openssh-server'
-    $service_name = 'sshd'
-  }
-  else {
-    fail("$facts['operatingsystem']} is not supported!")
-  }
+#unless $facts['os']['family'] == 'Debian' {
+#  $package_name = 'openssh-server'
+#  $service_name = 'sshd'
+#}
+
+#  if $facts['os']['family'] == 'Debian' {
+#    $package_name = 'openssh-server'
+#    $service_name = 'ssh'
+#  }
+#  elsif $facts['os']['family'] == 'Redhat' {
+#    $package_name = 'openssh-server'
+#    $service_name = 'sshd'
+#  }
+#  else {
+#    fail("$facts['operatingsystem']} is not supported!")
+#  }
 
 #  case $facts['os']['family'] {
-#    'Debian': {
-#      $package_name = 'openssh-server'
-#      $service_name = 'ssh'
-#    }
-#    'Redhat': {
-#      $package_name = 'openssh-server'
-#      $service_name = 'sshd'
-#   }
-#   default: {
-#     fail("$facts['operatingsystem']} is not supported!")
-#    }
-#  }
+  case $facts['operatingsystem'] {
+    'Debian', 'Ubuntu': {
+      $package_name = 'openssh-server'
+      $service_name = 'ssh'
+    }
+    /^RedHat|CentOS/: {
+      $package_name = 'openssh-server'
+      $service_name = 'sshd'
+      notify{ "${0} is our operating system!": }
+   }
+   default: {
+     fail("$facts['operatingsystem']} is not supported!")
+    }
+  }
 }
