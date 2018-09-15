e2bfaeb40a69466bfd1e9c74b0a96ba62859fcdd
root <root@bryanc1.mylabserver.com>
2018-09-15 12:25:27 +0000

    control ssh-service

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,9 @@
 #
 # Copyright 2018 Your name here, unless otherwise noted.
 #
-class ssh {
-
-
-}
+class ssh(
+  String $package_name = $::ssh::params::package_name,
+  String $service_name = $::ssh::params::service_name,
+) inherits ::ssh::params {
+  class { '::ssh::install': } ->
+  class { '::ssh::service': }}
