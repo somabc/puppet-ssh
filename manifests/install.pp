19c75639c6e71c799f44a3f27bfd565cba216159
root <root@bryanc1.mylabserver.com>
2018-09-15 17:13:05 +0000

    if debian

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,7 @@
 class ssh::install(
   String $package_name = $::ssh::package_name,
 ) {
-  package { 'ssh-package':
+  package { 'ssh_package':
     ensure => present,
     name   => $package_name,
   }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,27 @@
 class ssh::params {
-  case $facts['os']['family'] {
-    'Debian': {
-      $package_name = 'openssh-server'
-      $service_name = 'ssh'
-    }
-    'Redhat': {
-      $package_name = 'openssh-server'
-      $service_name = 'sshd'
-   }
-   default: {
-     fail("$facts['operatingsystem']} is not supported!")
-    }
+  if $facts['os']['family'] == 'Debian' {
+    $package_name = 'openssh-server'
+    $service_name = 'ssh'
   }
+  elsif $facts['os']['family'] == 'Redhat' {
+    $package_name = 'openssh-server'
+    $service_name = 'sshd'
+  }
+  else {
+    fail("$facts['operatingsystem']} is not supported!")
+  }
+
+#  case $facts['os']['family'] {
+#    'Debian': {
+#      $package_name = 'openssh-server'
+#      $service_name = 'ssh'
+#    }
+#    'Redhat': {
+#      $package_name = 'openssh-server'
+#      $service_name = 'sshd'
+#   }
+#   default: {
+#     fail("$facts['operatingsystem']} is not supported!")
+#    }
+#  }
 }
